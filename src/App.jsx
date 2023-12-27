148d07f29dcc26c80ce6ce7ea6b6a3c6393a97e0
IgorPanasik <panasik.igor.com@gmail.com>
2023-12-27 22:31:19 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import CurrencyRow from "./components/currency-row/CurrencyRow";
+
+import "./style.css";
+import "./components/currency-row/container-input.css";
+
+const BASE_URL = "http://apilayer.net/api/live";
+const access_key = "f864919e4b93ad4484dff5e99ab59a3a";
+
+function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeQuote, setExhangeQuote] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeQuote;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeQuote;
+  }
+
+  useEffect(() => {
+    fetch(
+      `${BASE_URL}?access_key=${access_key}&currencies=USD,EUR,JPY,GBP,AUD,CAD,CHF,CNY,SEK,NZD,BYN,RUB&source=USD&format=1`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.quotes) {
+          const firstCurrency = Object.keys(data.quotes)[0].slice(3);
+          setCurrencyOptions([
+            data.source,
+            ...Object.keys(data.quotes).map((key) => key.slice(3)),
+          ]);
+          setFromCurrency(data.source);
+          setToCurrency(firstCurrency);
+          setExhangeQuote(data.quotes["USD" + firstCurrency]);
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(
+        `${BASE_URL}?access_key=${access_key}&source=${fromCurrency}&currencies=${toCurrency}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data && data.quotes) {
+            setExhangeQuote(data.quotes[fromCurrency + toCurrency]);
+          }
+        });
+    }
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(event) {
+    setAmount(event.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(event) {
+    setAmount(event.target.value);
+    setAmountInFromCurrency(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>Currency Converter</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(option) => setFromCurrency(option)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
+      <div className="equals">↑↓</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(option) => setToCurrency(option)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/currency-row/CurrencyRow.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Select from "react-select";
+import ReactCountryFlag from "react-country-flag";
+
+export default function CurrencyRow({
+  currencyOptions,
+  selectedCurrency,
+  onChangeCurrency,
+  onChangeAmount,
+  amount,
+}) {
+  const options = currencyOptions.map((option) => ({
+    value: option,
+    label: (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ReactCountryFlag
+          countryCode={option.slice(0, 2)}
+          svg
+          style={{ marginRight: "5px" }}
+        />
+        {option}
+      </div>
+    ),
+  }));
+
+  return (
+    <div className="container-input">
+      <input
+        type="number"
+        className="input"
+        value={amount || ""}
+        onChange={onChangeAmount}
+      />
+      <Select
+        value={options.find((option) => option.value === selectedCurrency)}
+        onChange={(option) => onChangeCurrency(option ? option.value : "")}
+        options={options}
+        styles={{
+          control: (provided) => ({
+            ...provided,
+            cursor: "pointer",
+          }),
+        }}
+      />
+    </div>
+  );
+}
