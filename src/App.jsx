2b31f3dc178da45dda93b0b0a644563f2eae015b
IgorPanasik <panasik.igor.com@gmail.com>
2023-12-31 16:08:43 +0300

    fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -96,12 +96,11 @@ function App() {
       />
       <div className="equals">
         <svg
-          className="svg-equals"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke-width="1.5"
-          stroke="currentColor"
+          stroke="rgb(65, 130, 250)"
           class="w-6 h-6"
         >
           <path
